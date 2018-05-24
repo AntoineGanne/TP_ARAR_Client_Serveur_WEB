d3e8d28deeab0de2da75cf33937a7d6688918745
Antoine Ganne <antoine.ganne@hotmail.fr>
2018-05-24 11:22:27 +0200

    connexion TCP client serveur basique

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+    Socket connexionServ;
+    InputStream inC;
+    OutputStream outC;
+
+    private static int portServeur=80;
+
+    private static String ipServeur="127.0.0.1";
+
+    public static void main(String[] args){
+        Client c=new Client();
+        c.connexion();
+        c.fermerConnexion();
+    }
+
+
+    Client(){
+
+    }
+
+    private void connexion(){
+        try {
+            connexionServ =new Socket(ipServeur,portServeur);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void fermerConnexion(){
+        try {
+            connexionServ.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/Serveur.java
+++ src/Serveur.java
@@ -1,2 +1,40 @@
-public class Serveur extends Util{
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Serveur {
+    private ServerSocket socServ;
+    private Socket connexionClient;
+
+    private static int portServeur=80;
+
+    public static void main(String[] args){
+        Serveur s=new Serveur();
+        s.connexion();
+        s.fermerConnexion();
+    }
+
+    public Serveur(){
+
+    }
+
+    private void connexion(){
+        try {
+            socServ =new ServerSocket(portServeur);
+            connexionClient = socServ.accept();
+            System.out.println("connexion acceptée"+ connexionClient.getInetAddress()+ " port: "+ connexionClient.getPort());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fermerConnexion(){
+        try {
+            socServ.close();
+            connexionClient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
