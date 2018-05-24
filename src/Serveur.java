e4a40a16939cbf0c2f89c3d55e599f5051783707
Antoine Ganne <antoine.ganne@hotmail.fr>
2018-05-24 11:48:48 +0200

    test connexion

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -6,11 +8,19 @@ public class Serveur {
     private ServerSocket socServ;
     private Socket connexionClient;
 
+
+    InputStream inS;
+    OutputStream outS;
+
     private static int portServeur=80;
 
     public static void main(String[] args){
         Serveur s=new Serveur();
         s.connexion();
+        s.initialiserStreams();
+        s.ecouterClient();
+
+
         s.fermerConnexion();
     }
 
@@ -35,6 +45,29 @@ public class Serveur {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void initialiserStreams(){
+        try {
+            inS=connexionClient.getInputStream();
+            outS=connexionClient.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String ecouterClient(){
+        try {
+            char s=(char)inS.read();
+            while(s!=-1){
+               System.out.print(s);
+               s=(char)inS.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "yo yo yo";
     }
 
 }
