f8fa66cd956b051b3c2a4a657864c078485f1e25
Omegabug <vial.jeanvial@gmail.com>
2018-05-24 11:23:07 +0200

    Lecture fichier

--- src/Serveur.java
+++ src/Serveur.java
@@ -1,2 +1,31 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.IOException;
+import java.lang.String;
+import java.lang.Byte;
+
 public class Serveur extends Util{
+
+    public List<Byte> lectureFichier(String adressseFichier){
+        List<Byte>  input =new ArrayList<Byte>();
+        int i,b;
+        try{
+            FileInputStream f= new FileInputStream(adressseFichier);
+            b=f.read();
+            for (i = 0; b!=-1; i++) {
+
+                input.add((byte)b);
+                b=f.read();
+            }
+
+
+        }
+        catch(IOException ex){
+            System.out.println(ex);
+        }
+
+        System.out.println();
+    }
 }
