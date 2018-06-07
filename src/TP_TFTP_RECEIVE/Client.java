bda4b288fc11e149d5c68353c1fa65b1c1b1ef44
Omegabug <vial.jeanvial@gmail.com>
2018-06-07 11:36:52 +0200

    Creation requete

--- src/TP_TFTP_RECEIVE/Client.java
+++ src/TP_TFTP_RECEIVE/Client.java
@@ -1,21 +1,62 @@
 package TP_TFTP_RECEIVE;
 
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+//Inspiré de https://javapapers.com/java/java-tftp-client/
 public class Client {
     DatagramSocket ds;
-    static final int portTFTP=69;
-    static final int SUCCESS=0;
-    static final int LOCAL_ERROR=-1;
-    static final int TRANSFERT_ERROR=1;
-
+    private static final int portTFTP=69;
+    private static final int SUCCESS=0;
+    private static final int LOCAL_ERROR=-1;
+    private static final int TRANSFERT_ERROR=1;
+    private  static final byte RRQ=1;
+    private  static final byte DATA=2;
+    private  static final byte ACK=3;
+    private  static final byte ERROR=3;
 
     public static void main (String[] arg){
-
+        byte[] rq=faireDemandeFichier("Coucou.txt");
+        try {
+            String lisible = new String(rq, "UTF-8");
+            System.out.println(lisible);
+            rq
+        }
+        catch(IOException e){
+            System.out.println("BUg");
+        }
     }
 
-    public short SendFile(String nomFichierLocal,String adresseDistante){
+    public short receiveFile(String nomFichierLocal,String adresseDistante){
+        return 0;
+    }
+    //Il crée une requète RRQ pour le fichier donné en argument, le mode est octet
+    static public byte[] faireDemandeFichier(String nomFichier){
+        String mode="octet";
+        int requeteTaille=4+nomFichier.length()+mode.length();
+
+        byte[] requete=new byte[requeteTaille];
+
+        requete[0]=(byte)0;
+        requete[1]=RRQ;
+         //On rajoute le nom du fichier à la requête
+        System.arraycopy(nomFichier.getBytes(),0,requete,2,nomFichier.length());
+
+        requete[3+nomFichier.length()]=(byte)0;
+
+
+        System.arraycopy(mode.getBytes(),0,requete,3+nomFichier.length(),mode.length());
+
+        requete[requeteTaille-1]=(byte)0;
+        return requete;
+
 
     }
 }
